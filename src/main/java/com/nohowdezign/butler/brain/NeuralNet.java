997d0d2322b6de2f8b54a524bf8f36b17f1a3d39
nh-99 <ifence9@gmail.com>
2016-06-23 13:56:03 -0400

    WIP Neural net changes

--- src/main/java/com/nohowdezign/butler/brain/NeuralNet.java
+++ src/main/java/com/nohowdezign/butler/brain/NeuralNet.java
@@ -1,7 +1,14 @@
 package com.nohowdezign.butler.brain;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * @author Noah Howard
  */
 public class NeuralNet {
+    // Maps a subject string, right or wrong to a return query
+    private HashMap<HashMap<String, Class>, Boolean> subjectList = new HashMap<>();
+
+
 }
